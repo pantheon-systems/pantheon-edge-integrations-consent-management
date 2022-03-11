125fa6dda337c86631cbe3a984f8bbe00b8b32ff
Chris Reynolds <chris@jazzsequence.com>
2022-03-11 14:40:57 -0700

    register the interest cookie and localstorage with the consent api

--- wp-content/plugins/pantheon-edge-integrations-consent-management/inc/namespace.php
+++ wp-content/plugins/pantheon-edge-integrations-consent-management/inc/namespace.php
@@ -7,6 +7,7 @@
 
 namespace Pantheon\EI\Consent;
 
+use Pantheon\EI\WP\Interest;
 
 /**
  * Bootstrap the plugin.
@@ -17,3 +18,27 @@ function bootstrap() {
 	add_filter( "wp_consent_api_registered_$plugin", '__return_true' );
 }
 
+function register_cookies() {
+	if ( function_exists( 'wp_add_cookie_info' ) ) {
+		// Register the Interest cookie.
+		wp_add_cookie_info(
+			'interest', // The name of the cookie.
+			__( 'Pantheon WordPress Edge Integrations', 'pantheon-edge-integrations-consent-management' ), // The plugin or service that sets the cookie.
+			'marketing', // The type of cookie.
+			Interest\get_cookie_expiration(), // The expiration time.
+			'User interest data.' // What the cookie is meant to do.
+		);
+
+		wp_add_cookie_info(
+			'pantheon_ei.interest', // The name of the cookie.
+			__( 'Pantheon WordPress Edge Integrations', 'pantheon-edge-integrations-consent-management' ), // The plugin or service that sets the cookie.
+			'marketing', // The type of cookie.
+			Interest\get_cookie_expiration(), // The expiration time.
+			'User interest data.', // What the cookie is meant to do.
+			'Interest tracking', // The type of personal data that is collected.
+			false, // Not a member cookie.
+			false, // Not an administrator-only cookie.
+			'LOCALSTORAGE' // The cookie type.
+		);
+	}
+}
