3bd2bf5cbed9762cbc921bd2ce27157297dec1a8
Chris Reynolds <chris@jazzsequence.com>
2022-03-11 14:40:30 -0700

    kick off the plugin and register edge integrations with the api

--- wp-content/plugins/pantheon-edge-integrations-consent-management/inc/namespace.php
+++ wp-content/plugins/pantheon-edge-integrations-consent-management/inc/namespace.php
@@ -7,4 +7,13 @@
 
 namespace Pantheon\EI\Consent;
 
-function bootstrap() {}
+
+/**
+ * Bootstrap the plugin.
+ */
+function bootstrap() {
+	// Register the EI plugin with the Consent API.
+	$plugin = WP_PLUGIN_DIR . '/pantheon-wordpress-edge-integrations/pantheon-wordpress-edge-integrations.php';
+	add_filter( "wp_consent_api_registered_$plugin", '__return_true' );
+}
+
