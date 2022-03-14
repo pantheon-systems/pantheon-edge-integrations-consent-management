c1bf60af45a258508485889bf0cb4b535dd023bd
Chris Reynolds <chris@jazzsequence.com>
2022-03-14 15:56:51 -0600

    add a "preferences updated" template part

---
+++ wp-content/plugins/pantheon-edge-integrations-consent-management/templates/part-updated.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Consent Updated Message
+ *
+ * @package Pantheon/EdgeIntegrations/ConsentManagement
+ */
+
+?>
+<div class="cookie-consent-banner__updated">
+	<p>
+		<span class="cookie-consent-banner__updated__message">
+			<?php esc_html_e( 'Cookie preferences updated.', 'pantheon-edge-integrations-consent-management' ); ?>
+		</span>
+		<button class="cookie-consent-banner__updated-close">
+			<?php esc_html_e( 'Close', 'pantheon-edge-integrations-consent-management' ); ?>
+		</button>
+	</p>
+</div>
