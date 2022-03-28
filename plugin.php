67ac99d7f315cc7a8902475433e705dc618e9ea0
Chris Reynolds <chris@jazzsequence.com>
2022-03-28 10:23:53 -0600

    add -dev to plugin file

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Pantheon Edge Integrations Consent Management
  * Description: Implements WP Consent API into Pantheon Edge Integrations to manage consent and data tracking.
- * Version: 0.1.5
+ * Version: 0.1.5-dev
  * Author: Pantheon
  * Author URI: https://pantheon.io
  * Requires at least: 5.0
