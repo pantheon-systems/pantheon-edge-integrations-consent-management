2ac823cd4556ae0066734a79d6c9d9beab8fbe0f
Chris Reynolds <chris@jazzsequence.com>
2022-03-31 12:08:18 -0600

    add tests for the other helper functions

--- tests/mainTest.php
+++ tests/mainTest.php
@@ -16,5 +16,24 @@ class testsMain extends TestCase {
 		$this->assertTrue( function_exists( 'wp_consent_api_activation_check' ) );
 		$this->assertTrue( function_exists( 'wp_has_consent' ) );
 	}
+
+	public function testDoNotSendVaryHeaders() {
+		$vary_headers = do_not_send_vary_headers();
+
+		$this->assertTrue( is_array( $vary_headers ) );
+		$this->assertFalse( $vary_headers['Audience'] );
+		$this->assertFalse( $vary_headers['Audience-Set'] );
+		$this->assertFalse( $vary_headers['Interest'] );
+	}
+
+	public function testDoNotAllowAnyPostTypes() {
+		$post_types = do_not_allow_any_post_types();
+
+		$this->assertTrue( is_array( $post_types ) );
+		$this->assertContains( 'none', $post_types );
+	}
+
+	public function testSetConsentType() {
+		$this->assertEquals( 'optin', set_consent_type() );
 	}
 }
