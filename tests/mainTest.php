43960b373b0eee993cd7bc1df48eda4d409e86fc
Chris Reynolds <chris@jazzsequence.com>
2022-03-31 12:08:09 -0600

    update the api is loaded test

--- tests/mainTest.php
+++ tests/mainTest.php
@@ -4,10 +4,17 @@ namespace Pantheon\EI\Consent;
 use PHPUnit\Framework\TestCase;
 
 class testsMain extends TestCase {
+	protected function setUp() : void {
+		parent::setUp();
+		// Load the consent api plugin.
+		$consent_api_path = dirname( __DIR__ ) . '/vendor/rlankhorst/wp-consent-level-api/';
+		require_once "$consent_api_path/wp-consent-api.php";
+		require_once "$consent_api_path/api.php";
+	}
+
 	public function testWPConsentAPIisLoaded() {
-		$this->assertFalse( class_exists( 'WP_CONSENT_API' ) );
-		// Run the maybe_require_wp_consent_api function and maybe require it.
-		maybe_require_wp_consent_api();
-		$this->assertTrue( class_exists( 'WP_CONSENT_API' ) );
+		$this->assertTrue( function_exists( 'wp_consent_api_activation_check' ) );
+		$this->assertTrue( function_exists( 'wp_has_consent' ) );
+	}
 	}
 }
